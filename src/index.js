3f4f168494f01c1c01613afcb092c06ff46ba105
Donovan <cotterdonovan@gmail.com>
2021-09-01 23:11:35 -0400

    v1.0.3

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const mongoPassword = process.env.MONGO_PASSWORD;
 const mongoDatabase = 'contacts';
 const connectionString = `mongodb+srv://dcotter:${mongoPassword}@cluster0.93f0r.mongodb.net/${mongoDatabase}?retryWrites=true&w=majority`;
 
+console.log('==================== mongoPassword ========================');
+console.log(mongoPassword);
+console.log('===================== mongoPassword =======================');
+
 mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
 	const app = express();
 	app.use(express.json());
